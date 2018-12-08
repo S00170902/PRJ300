24c712e50e9506aee6972e5c9228bde5d2dda977
Fiona Davey <S00170902@mail.itsligo.ie>
2018-12-08 15:46:48 +0000

    Removed unnecessary model

--- PRJ300/WebForm1.aspx.cs
+++ PRJ300/WebForm1.aspx.cs
@@ -12,7 +12,6 @@ namespace PRJ300
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        public static S00171672Entities db = new S00171672Entities();
         protected void Page_Load(object sender, EventArgs e)
         {
             
@@ -39,19 +38,6 @@ namespace PRJ300
 
         protected void onClick(object sender, EventArgs e)
         {
-            //GridView1.DataSource = null;
-            //GridView2.DataSource = null;
-            //var query1 = from c in db.DummyTables
-            //             where c.Name == null || c.price == null
-            //             select c;
-
-            //var query2 = from c in db.DummyTable2
-            //             where c.Name == null || c.price == null
-            //             select c;
-
-            //GridView1.DataSource = query1.ToList();
-            //GridView2.DataSource = query2.ToList();
-
             using (SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["S00171672ConnectionString"].ToString()))
             {
                 sqlcon.Open();
